60a7b7c2ee5399d1b2ba100b92dd77c91104bca8
Chris Wolfe <chriswwolfe@gmail.com>
2016-10-27 09:59:35 -0500

    remove unneeded calls

--- test/ints/core_test.clj
+++ test/ints/core_test.clj
@@ -3,7 +3,5 @@
             [ints.core :as core]))
 
 (deftest test-roundtrip
-  (let [a (bigint 12)
-        cin (core/bigint->b64 a)
-        cout (core/b64->bigint cin)] ;; should be random!
+  (let [a (bigint 12)] ;; should be random!
     (is (= a (core/b64->bigint (core/bigint->b64 a))))))
